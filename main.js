d0b7df3b1e8a8e9249fd539a05993e10eed91562
Svyshch bc2024-6 <den.svyshch@gmail.com>
2024-11-24 22:16:11 +0200

    Відлагоджено роботу сервісу та додано документацію.

--- main.js
+++ main.js
@@ -21,9 +21,27 @@ if (!fs.existsSync(cacheDir)) {
   fs.mkdirSync(cacheDir, { recursive: true });
 }
 
-app.use(express.json());
-const upload = multer();
+app.use(express.json()); // мідлвар для обробки сирого тексту
+const upload = multer(); // мідлвар для роботи з multipart/form-data
 
+/**
+ * @openapi
+ * /notes/{note}:
+ *   get:
+ *     description: Відповідає за одержання нотатки.
+ *     parameters:
+ *       - name: note
+ *         in: path
+ *         description: Назва нотатки
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Текст нотатки був успішно одержаний.
+ *       400:
+ *         description: Нотатки не знайдено.
+ */
 app.get('/notes/:noteName', (req, res) => {
     const notePath = path.join(cacheDir, req.params.noteName + '.txt');
     if (!fs.existsSync(notePath)) {
@@ -33,6 +51,31 @@ app.get('/notes/:noteName', (req, res) => {
     res.send(noteContent);
   });
 
+ /**
+ * @openapi
+ * /notes/{note}:
+ *   put:
+ *     description: Відповідає за оновлення нотатки.
+ *     parameters:
+ *       - name: note
+ *         in: path
+ *         description: Назва нотатки
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         text/plain:
+ *           schema:
+ *             type: string
+ *             description: Текст нотатки
+ *     responses:
+ *       200:
+ *         description: Текст нотатки був вдало змінений.
+ *       400:
+ *         description: Якщо не було знайдено відповідну нотатку.
+ */ 
   app.put('/notes/:noteName', express.text(), (req, res) => {
     const notePath = path.join(cacheDir, req.params.noteName + '.txt');
     if (!fs.existsSync(notePath)) {
@@ -42,6 +85,24 @@ app.get('/notes/:noteName', (req, res) => {
     res.send('Note updated');
   });
 
+  /**
+ * @openapi
+ * /notes/{note}:
+ *   delete:
+ *     description: Відповідає за видалення нотатки.
+ *     parameters:
+ *       - name: note
+ *         in: path
+ *         description: Назва нотатки
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       201:
+ *         description: Нотатка була вдало видалена.
+ *       400:
+ *         description: Нотатки для видалення не знайдено.
+ */
   app.delete('/notes/:noteName', (req, res) => {
     const notePath = path.join(cacheDir, req.params.noteName + '.txt');
     if (!fs.existsSync(notePath)) {
@@ -51,6 +112,15 @@ app.get('/notes/:noteName', (req, res) => {
     res.send('Note deleted');
   });
 
+  /**
+ * @openapi
+ * /notes:
+ *   get:
+ *     description: Відповідає за одержання JSON з нотатками.
+ *     responses:
+ *       200:
+ *         description: JSON з нотатками був успішно одержаний.
+ */
   app.get('/notes', (req, res) => {
     const notes = fs.readdirSync(cacheDir).map(file => {
       const noteName = path.parse(file).name;
@@ -60,7 +130,31 @@ app.get('/notes/:noteName', (req, res) => {
     res.json(notes);
   });
 
-  app.post('/write', upload.none(), (req, res) => {
+  /**
+ * @openapi
+ * /write:
+ *   post:
+ *     description: Відповідає за додавання нової нотатки.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               note_name:
+ *                 type: string
+ *                 description: назва нотатки
+ *               note:
+ *                 type: string
+ *                 description: текст нотатки
+ *     responses:
+ *       201:
+ *         description: У разі вдалого створення нотатки.
+ *       400:
+ *         description: Некоректний запит у разі існування такої нотатки.
+ */
+  app.post('/write', upload.none(), (req, res) => {   //метод none() то значить файли відсутні, працюємо тільки з текстом
     const { note_name, note } = req.body;
     const notePath = path.join(cacheDir, note_name + '.txt');
     
@@ -71,10 +165,40 @@ app.get('/notes/:noteName', (req, res) => {
     fs.writeFileSync(notePath, note);
     res.status(201).send('Note created');
   });
-  
+ 
+/**
+ * @openapi
+ * /UploadForm.html:
+ *   get:
+ *     description: Відповідає за одержання форми для додавання нової нотатки.
+ *     responses:
+ *       200:
+ *         description: Форма з нотатками була успішно одержана.
+ *       500:
+ *         description: Форма з нотатками загубилась.
+ */  
   app.get('/UploadForm.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'UploadForm.html'));
   });
+
+  //<swagger>
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options_swagger = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Моя документація для нотаток',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./main.js'],
+};
+
+const openapiSpecification = swaggerJsdoc(options_swagger);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+//</swagger>
 
 // Запуск сервера
 app.listen(options.port, options.host, () => {
